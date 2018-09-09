02cba1007e0876c1340affcff236873eacc016b4
BuGyeom Im <21100582@handong.edu>
2018-09-09 14:18:47 +0000

    YHCP21-22

---
+++ 20180909/YHCP22.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+
+
+/*
+Q1.
+
+int num1, num2, num3, num4, num5;
+
+
+int main(void)
+{
+    std::cout<<"insert first integer: " ;
+    std::cin>>num1;
+    
+    std::cout<<"insert second integer: ";
+    std::cin>>num2;
+    
+    std::cout<<"insert third integer: ";
+    std::cin>>num3;
+    
+    std::cout<<"insert fourth integer: ";
+    std::cin>>num4;
+    
+    std::cout<<"insert fifth integer: ";
+    std::cin>>num5;
+    
+    
+    std::cout<<"SUM: "<<num1+num2+num3+num4+num5<<std::endl;
+    
+    return 0;
+    
+    
+}
+
+*/
+
+
+
+
+
+/*
+Q2.
+
+char name[100], num[100];
+
+int main(void)
+{
+    std::cout<<"What is your name?: ";
+    std::cin>>name;
+    
+    std::cout<<"What is your phone number?: ";
+    std::cin>>num;
+    
+    std::cout<<name<<std::endl;
+    std::cout<<num<<std::endl;
+    
+    return 0;
+    
+    
+}
+
+*/
+
+
+
+
+/*
+Q3.
+
+
+int num1, result;
+int num2 = 1;
+
+
+int main(void)
+{
+    std::cout<<"which table?: ";
+    std::cin>>num1;
+    
+    
+    if (num2<)
+    for (num2; num2 < 10; num2++)
+        result = num1 * num2;
+        
+        std::cout<<"num1 x num2 = "<<result<<std::endl;
+        
+    return 0;
+}
+
+*/
