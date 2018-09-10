dc40c21720e225c07f3bc95b29b09d8b58f214ed
BuGyeom Im <21100582@handong.edu>
2018-09-10 15:39:55 +0000

    practice180910

---
+++ 20180910/M_S_practice.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+
+
+char id1, id2, id3;
+int ad1, ad2, ad3;
+int val1, val2, val3;
+
+
+int main(void)
+{
+    id1 = '3';
+    
+    
+    std::cout<<id1<<std::endl;
+    
+    int(id1);
+    std::cout<<id1<<std::endl;
+    
+    
+    
+    
+    return 0;
+    
+    
+    
+    
+    
+    
+    
+    
+}
