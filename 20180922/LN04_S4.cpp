756986d341da5df44b60c4a041d1904a54838bb9
BuGyeom Im <21100582@handong.edu>
2018-09-22 15:07:28 +0000

    test

--- 20180922/LN04_S4.cpp
+++ 20180922/LN04_S4.cpp
@@ -30,6 +30,9 @@ int main(int argc, char** argv)
     }
     
     
+    
+    
+    
     for (i = 1; i <= 10; i ++)
-        std::cout<<("[%2d] (%3d/100) %c\n", i, scores[i], grades[i]);
+        std::cout<<("[%2d] (%3d/100) %c\n", i, scores[i], grades[i])<<std::endl;
 }
