6da40569ceb1244df007c4c919022febdacaba31
BuGyeom Im <21100582@handong.edu>
2018-10-09 12:26:44 +0000

    forgot...

---
+++ 20181008/lecture.cpp
@@ -0,0 +1,32 @@
+/*
+
+struct tag_name
+{
+    type1 name1;
+    type2 name2;
+};
+
+*/
+
+
+
+
+struct student
+{
+    int _id;
+    int _score;
+    char _name[20];
+    
+    
+};
+
+
+struct student
+{
+    int _id;
+    int _score;
+    char _name[20];
+    
+    
+} stu;
+//위 타입의 stu라는 변수 생성.
