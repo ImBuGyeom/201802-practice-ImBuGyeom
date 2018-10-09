4f5f081de72930f752615b772515825c645ab0eb
BuGyeom Im <21100582@handong.edu>
2018-10-09 15:30:02 +0000

    hgr

---
+++ 20181009/try.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+int main()
+{
+    
+    
+    char a = '0';
+    char b = 0;
+
+    std::cout<<"a1 "<<a<<std::endl;
+    std::cout<<"b1 "<<b<<std::endl;    
+    std::cout<<"a2 "<<a+1<<std::endl;
+    std::cout<<"b2 "<<b+1<<std::endl<<std::endl;
+    
+    
+    
+    a = a+1;
+    b = b+1;
+    
+    std::cout<<"a3 "<<a<<std::endl;
+    std::cout<<"b3 "<<b<<std::endl;
+    std::cout<<"a4 "<<a+1<<std::endl;   
+    std::cout<<"b4 "<<b+1<<std::endl;
+    
+    
+}
